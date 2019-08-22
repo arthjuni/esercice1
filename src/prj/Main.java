75db01327251eb4846a22f0aa217a990aaa17903
Administrator <Administrator@DESKTOP-A6HEQ1V>
2019-08-22 17:43:16 +0200

    la ca viens de ajouter un classpath

--- src/prj/Main.java
+++ src/prj/Main.java
@@ -1,9 +1,10 @@
 package prj;
 	
-import javafx.application.Application;
+
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 
 
